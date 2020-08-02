3e94f486c88c09d1930247b6695fc28b54dd14e4
Nur Hidayatullah <36223951+kematjaya0@users.noreply.github.com>
2020-08-02 09:38:40 +0700

    linux

--- src/Builder/SerialNumberBuilder.php
+++ src/Builder/SerialNumberBuilder.php
@@ -46,28 +46,7 @@ class SerialNumberBuilder implements SerialNumberBuilderInterface
     
     private function getLinuxMac()
     {
-        exec('netstat -ie', $result);
-        if(is_array($result)) 
-        {
-            $iface = array();
-            foreach($result as $key => $line) 
-            {
-                if($key > 0) {
-                    $tmp = str_replace(" ", "", substr($line, 0, 10));
-                    if($tmp <> "") 
-                    {
-                        $macpos = strpos($line, "HWaddr");
-                        if($macpos !== false) 
-                        {
-                            $iface[] = array('iface' => $tmp, 'mac' => strtolower(substr($line, $macpos+7, 17)));
-                        }
-                    }
-                }
-            }
-            return $iface[0]['mac'];
-        }
-        
-        throw new \Exception('not found');
+        return 'on-progress';
     }
     
     private function getMacAddress()
